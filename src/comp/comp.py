de583ec67679e991f916e1dbdced70e6125fe425
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-14 17:42:45 -0600

    starts between C and G, inclusive complete

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -36,7 +36,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [each.name for each in humans if ord(each.name[0]) in range(68, 71)]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
